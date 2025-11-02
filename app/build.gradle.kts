281cc99ee80d43fac7a1080d515c47ed72655ca1
Illia Datsiuk <ilya.datsiuk@gmail.com>
2025-11-02 13:36:35 +0100

    Updated minSDK -> 35 - Android 12 Updated Icon and texts for Astolfo's Player

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
 
     defaultConfig {
         applicationId = "com.dn0ne.lotus"
-        minSdk = 24
+        minSdk = 31
         targetSdk = 35
         versionCode = 1_002_000
         versionName = "1.2.0"
